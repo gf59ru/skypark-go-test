8561e985d2c70aa3940d892cffddf6e065848840
gf59ru <gf59ru@live.com>
2016-03-29 03:19:54 -0700

    First html view

--- skypark.go
+++ skypark.go
@@ -3,18 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 
-	"github.com/jinzhu/gorm"
+	//"github.com/jinzhu/gorm"
 )
+
+func welcome(c web.C, w http.ResponseWriter, r *http.Request) {
+	t := template.New("welcome")
+	t, _ = t.ParseFiles("views/welcome.html", "")
+	t.Execute(w, "")
+}
 
 func hello(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
 }
 
 func main() {
+	goji.Get("/", welcome)
 	goji.Get("/hello/:name", hello)
 	goji.Serve()
 }
